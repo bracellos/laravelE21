43243ae18a29f70595b707c15e96c92e489d1346
Diego Bracellos <bracellos@gmail.com>
2024-02-07 18:19:38 -0300

    blade

---
+++ resources/views/blog/artigos.blade.php
@@ -0,0 +1,10 @@
+@extends("layout.blog")
+@section("title", "Artigos do blog")
+@section("content")
+<h1>aula de programação</h1>
+<button class="btn btn-verdeCaganeira">AULA ONLINE</button>
+
+<div class="alert alert-primary blue" role="alert">
+    A simple primary alert—check it out!
+  </div>
+@endsection
